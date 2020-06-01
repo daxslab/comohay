074033dd925802a55dd4f5df09166030cfe4aaf4
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-06-01 12:09:22 -0400

    include ads user signature translations

--- ads/models/ad.py
+++ ads/models/ad.py
@@ -21,7 +21,7 @@ class Ad(BaseModel):
     external_id = models.CharField(max_length=200, blank=True, null=True, verbose_name=_('External ID'))
     external_url = models.URLField(blank=True, null=True, verbose_name=_('External URL'))
     created_by = CurrentUserField(verbose_name=_('Created by'))
-    updated_by = CurrentUserField(on_update=True, related_name='%(class)s_updated_by', verbose_name=_('Created at'))
+    updated_by = CurrentUserField(on_update=True, related_name='%(class)s_updated_by', verbose_name=_('Updated by'))
 
     class Meta:
         verbose_name = _('Ad')
