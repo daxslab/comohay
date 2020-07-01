9955564b42a1a67e48d72d40777bb226aa577e55
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-07-01 14:37:48 -0400

    new spiders for crawling ads

--- ads/importers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseImporter
-from .hogarencuba import HogarencubaImporter
-from .bachecubano import BachecubanoImporter
-from .porlalivre import PorlalivreImporter
-from .revolico import RevolicoImporter
