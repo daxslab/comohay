77b4c0b07a26b13a7cffdcdc3344a21134069434
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-07-15 12:05:18 -0400

    improve proxy crawler

--- scraper/spiders/proxycrawler.py
+++ scraper/spiders/proxycrawler.py
@@ -4,20 +4,10 @@ class ProxycrawlerSpider(scrapy.Spider):
     name = "proxycrawler"
 
     start_urls = [
-        'https://www.proxynova.com/proxy-server-list/',
+        'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
     ]
 
     def parse(self, response):
-        proxies = []
-        for proxy in response.css('table#tbl_proxy_list tbody tr'):
-            try:
-                proxies.append({
-                    'ip': proxy.css('td:nth-of-type(1) script::text').get().replace("document.write('", "").replace("');", "").strip(),
-                    'port': proxy.css('td:nth-of-type(2)::text').get().strip(),
-                })
-            except:
-                pass
         filename = 'proxies.txt'
         with open(filename, 'w') as f:
-            for proxy in proxies:
-                f.write(proxy['ip']+':'+proxy['port']+'\n')
+            f.write(str(response.text))
