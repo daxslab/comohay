65aa11f98da746e347ef68fffdca9747f9b0bd9e
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-07-22 17:05:47 -0400

    add subprocess pipe to crawl task

--- ads/tasks.py
+++ ads/tasks.py
@@ -6,5 +6,5 @@ from celery import shared_task
 @shared_task
 def crawl():
     # using subprocess.Popen instead of call_command because of celery issues with multiprocessing
-    subprocess.Popen(['python', 'manage.py', 'proxy_crawler'])
-    subprocess.Popen(['python', 'manage.py', 'crawl'])
+    subprocess.Popen(['python', 'manage.py', 'proxy_crawler'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.Popen(['python', 'manage.py', 'crawl'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
