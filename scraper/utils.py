30ee865644acc25a58bd89dbf0fea0f7ad71e5fd
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-07-23 15:03:02 -0400

    improve spider updater

---
+++ scraper/utils.py
@@ -0,0 +1,13 @@
+from comohay import settings
+
+def get_spider_name_by_source(external_source):
+    """
+    Returns a spider name by source according `settings.EXTERNAL_SOURCES`
+    :param external_source: External source
+    :type external_source: str
+    :return: Spider name
+    :rtype: str
+    """
+    for name, source in settings.EXTERNAL_SOURCES.items():
+        if source == external_source:
+            return name
