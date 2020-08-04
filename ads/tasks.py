cdae48aaf986780ac5bc9c7d910ef1ebf8f0a06b
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2020-08-03 21:32:25 -0400

    fix crawl task

--- ads/tasks.py
+++ ads/tasks.py
@@ -12,7 +12,7 @@ def update_index():
 def crawl():
     # using subprocess.Popen instead of call_command because of celery issues with multiprocessing
     subprocess.Popen(['python', 'manage.py', 'proxy_crawler'])
-    subprocess.Popen(['python', 'manage.py', 'crawl', '--depth' '10'])
+    subprocess.Popen(['python', 'manage.py', 'crawl', '--depth', '10'])
 
 
 @shared_task
