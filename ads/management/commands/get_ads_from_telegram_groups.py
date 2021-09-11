654661c6cf51baaf3cd819015f3b77ec164fdf2e
daniel <danielrodcaball@gmail.com>
2021-09-11 00:03:38 -0400

    - add: asynchronous telegram groups processing

--- ads/management/commands/get_ads_from_telegram_groups.py
+++ ads/management/commands/get_ads_from_telegram_groups.py
@@ -9,6 +9,7 @@ from ads.models import TelegramGroup, Ad
 import logging
 import comohay.settings
 from currencies.services import currencyad_service
+import asyncio
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +21,11 @@ class Command(BaseCommand):
     help = "Get messages from all telegram groups saved in db. By default will only be fetch the messages in the " \
            "last 10 minutes per each group. If you add \"--all-messages\" option all messages will be fetched. If you" \
            "add --groups <group-username> only the ads of the group will be fetched"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Telegram desktop sample keys, see https://docs.telethon.dev/en/latest/basic/signing-in.html
+        self.client = TelegramClient('anon', comohay.settings.TELEGRAM_API_ID, comohay.settings.TELEGRAM_API_HASH)
 
     def add_arguments(self, parser):
 
@@ -34,8 +40,6 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        # Telegram desktop sample keys, see https://docs.telethon.dev/en/latest/basic/signing-in.html
-        client = TelegramClient('anon', comohay.settings.TELEGRAM_API_ID, comohay.settings.TELEGRAM_API_HASH)
 
         if options['minutes_offset']:
             self.minutes_offset = options['minutes_offset']
@@ -54,102 +58,105 @@ class Command(BaseCommand):
         print("Fetching telegram messages ...")
 
         async def main():
+            await asyncio.gather(*[self.process_group(telegram_group, offset_datetime) for telegram_group in tg_groups])
 
-            # tg_groups = await sync_to_async(list)(TelegramGroup.objects.all())
-            for telegram_group in tg_groups:
-                try:
-                    async for message in client.iter_messages(
-                            entity="@{}".format(telegram_group.username),
-                            offset_date=offset_datetime,
-                            reverse=True,
-                            wait_time=5
-                    ):
-
-                        await message.get_sender()
-
-                        if message.text is None or \
-                                message.sender is None or \
-                                message.sender.deleted or \
-                                message.sender.bot or \
-                                message.sender.scam or \
-                                message.sender.fake or \
-                                not message.sender.username:
-                            continue
-
-                        # TODO:
-                        #  Here must go a more general classifier.
-                        #  Right now we only accept ads that match the regex for a currencyad
-                        matches = re.match(currencies.services.currencyad_service.regex, message.text, re.IGNORECASE)
-                        if not matches:
-                            continue
-
-                        ad = Ad(
-                            title=matches.group(0),
-                            category_id=87,  # id of category "Cambio de Moneda"
-                            description=message.text,
-                            province_id=telegram_group.province_id,
-                            contact_tg=message.sender.username,
-                            external_source="t.me/{group_username}".format(group_username=telegram_group.username),
-                            external_id=message.id,
-                            external_url=telegram_group.link + "/{id}".format(id=message.id),
-                            external_created_at=message.date,
-                            is_deleted=False
-                        )
-
-                        currencyad = currencies.services.currencyad_service.get_currencyad_from_ad(ad)
-                        if currencyad:
-
-                            # TODO: Check get_newest_similar_currencyads method
-                            if await sync_to_async(Ad.objects.filter(external_url=ad.external_url).count)() > 0 or\
-                                    await sync_to_async(currencyad_service.get_newest_similar_currencyads(currencyad).count)() > 0:
-                                continue
-
-                            await sync_to_async(currencyad_service.soft_delete_older_similar_currencyads)(currencyad)
-
-                            ad.currency_iso = currencyad.target_currency_iso
-                            ad.price = currencyad.price
-                            await sync_to_async(ad.save)()
-                            await sync_to_async(currencyad.save)()
-
-                            logger.info(
-                                "New Ad from Telegram message: {}, {}, {}, {}".format(
-                                    message.date,
-                                    telegram_group.username,
-                                    message.sender.username,
-                                    message.text,
-                                )
-                            )
-
-                            logger.info(
-                                "New CurrencyAd from Telegram message: {}/{} {}".format(
-                                    currencyad.source_currency_iso,
-                                    currencyad.target_currency_iso,
-                                    currencyad.price
-                                )
-                            )
-
-                        else:
-                            # right now this is not reachable because the classifier only detect currencyads
-                            if await sync_to_async(ads.services.ad_service.has_duplicates)(ad):
-                                # TODO: do something similar as in BaseAdPipeline
-                                continue
-
-                            await sync_to_async(ad.save)()
-
-                            logger.info(
-                                "New Ad from Telegram message: {}, {}, {}, {}".format(
-                                    message.date,
-                                    telegram_group.username,
-                                    message.sender.username,
-                                    message.text,
-                                )
-                            )
-
-                except Exception as e:
-                    logger.error(e)
-
-        with client:
+        with self.client:
             try:
-                client.loop.run_until_complete(main())
+                self.client.loop.run_until_complete(main())
             except Exception as e:
                 logger.error(e)
+
+    async def process_group(self, telegram_group: TelegramGroup, offset_datetime: datetime):
+        try:
+
+            print(telegram_group.username)
+
+            async for message in self.client.iter_messages(
+                    entity="@{}".format(telegram_group.username),
+                    offset_date=offset_datetime,
+                    reverse=True,
+                    wait_time=5
+            ):
+
+                await message.get_sender()
+
+                if message.text is None or \
+                        message.sender is None or \
+                        message.sender.deleted or \
+                        message.sender.bot or \
+                        message.sender.scam or \
+                        message.sender.fake or \
+                        not message.sender.username:
+                    continue
+
+                # TODO:
+                #  Here must go a more general classifier.
+                #  Right now we only accept ads that match the regex for a currencyad
+                matches = re.match(currencies.services.currencyad_service.regex, message.text, re.IGNORECASE)
+                if not matches:
+                    continue
+
+                ad = Ad(
+                    title=matches.group(0),
+                    category_id=87,  # id of category "Cambio de Moneda"
+                    description=message.text,
+                    province_id=telegram_group.province_id,
+                    contact_tg=message.sender.username,
+                    external_source="t.me/{group_username}".format(group_username=telegram_group.username),
+                    external_id=message.id,
+                    external_url=telegram_group.link + "/{id}".format(id=message.id),
+                    external_created_at=message.date,
+                    is_deleted=False
+                )
+
+                currencyad = currencies.services.currencyad_service.get_currencyad_from_ad(ad)
+                if currencyad:
+
+                    if await sync_to_async(Ad.objects.filter(external_url=ad.external_url).count)() > 0 or \
+                            await sync_to_async(
+                                currencyad_service.get_newest_similar_currencyads(currencyad).count)() > 0:
+                        continue
+
+                    await sync_to_async(currencyad_service.soft_delete_older_similar_currencyads)(currencyad)
+
+                    ad.currency_iso = currencyad.target_currency_iso
+                    ad.price = currencyad.price
+                    await sync_to_async(ad.save)()
+                    await sync_to_async(currencyad.save)()
+
+                    logger.info(
+                        "New Ad from Telegram message: {}, {}, {}, {}".format(
+                            message.date,
+                            telegram_group.username,
+                            message.sender.username,
+                            message.text,
+                        )
+                    )
+
+                    logger.info(
+                        "New CurrencyAd from Telegram message: {}/{} {}".format(
+                            currencyad.source_currency_iso,
+                            currencyad.target_currency_iso,
+                            currencyad.price
+                        )
+                    )
+
+                else:
+                    # right now this is not reachable because the classifier only detect currencyads
+                    if await sync_to_async(ads.services.ad_service.has_duplicates)(ad):
+                        # TODO: do something similar as in BaseAdPipeline
+                        continue
+
+                    await sync_to_async(ad.save)()
+
+                    logger.info(
+                        "New Ad from Telegram message: {}, {}, {}, {}".format(
+                            message.date,
+                            telegram_group.username,
+                            message.sender.username,
+                            message.text,
+                        )
+                    )
+
+        except Exception as e:
+            logger.error(e)
